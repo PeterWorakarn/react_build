b6d0448537e429422584e60bc9a8ba96262c5fa1
Peter <skboardgames@gmail.com>
2020-04-20 12:26:13 +0700

    Update path

---
+++ precache-manifest.014c6bab7443d2f616b6995d602b2bf8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "454d515944435d19ecb5950671aef325",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f01994fe07ff25b2a45",
+    "url": "/static/css/main.7c5a0a98.chunk.css"
+  },
+  {
+    "revision": "b049852e5c8aa47d25d8",
+    "url": "/static/js/2.b8648606.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b8648606.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f01994fe07ff25b2a45",
+    "url": "/static/js/main.2ecd1061.chunk.js"
+  },
+  {
+    "revision": "53958c2f5f445d4f256c",
+    "url": "/static/js/runtime-main.3073377d.js"
+  },
+  {
+    "revision": "6743eda8aa14942840324d7b24b202fd",
+    "url": "/static/media/1.6743eda8.png"
+  },
+  {
+    "revision": "6743eda8aa14942840324d7b24b202fd",
+    "url": "/static/media/2.6743eda8.png"
+  },
+  {
+    "revision": "6743eda8aa14942840324d7b24b202fd",
+    "url": "/static/media/3.6743eda8.png"
+  },
+  {
+    "revision": "6743eda8aa14942840324d7b24b202fd",
+    "url": "/static/media/4.6743eda8.png"
+  },
+  {
+    "revision": "6743eda8aa14942840324d7b24b202fd",
+    "url": "/static/media/5.6743eda8.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ precache-manifest.56a4f519e883e2baa46b95b95f4df9a2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7999ef1ff6ce0b61cff474e289961fbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5d38f98baf713e6f5f8",
+    "url": "/static/css/main.7c5a0a98.chunk.css"
+  },
+  {
+    "revision": "1ffa14770327141fd53a",
+    "url": "/static/js/2.2bd39468.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2bd39468.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5d38f98baf713e6f5f8",
+    "url": "/static/js/main.7490c914.chunk.js"
+  },
+  {
+    "revision": "53958c2f5f445d4f256c",
+    "url": "/static/js/runtime-main.3073377d.js"
+  },
+  {
+    "revision": "6743eda8aa14942840324d7b24b202fd",
+    "url": "/static/media/1.6743eda8.png"
+  },
+  {
+    "revision": "6743eda8aa14942840324d7b24b202fd",
+    "url": "/static/media/2.6743eda8.png"
+  },
+  {
+    "revision": "6743eda8aa14942840324d7b24b202fd",
+    "url": "/static/media/3.6743eda8.png"
+  },
+  {
+    "revision": "6743eda8aa14942840324d7b24b202fd",
+    "url": "/static/media/4.6743eda8.png"
+  },
+  {
+    "revision": "6743eda8aa14942840324d7b24b202fd",
+    "url": "/static/media/5.6743eda8.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
